8e0835b875089924ecc36e4ac752680259b47e92
hiromichi_tsuda <tsuda@iti-inc.co.jp>
2020-05-20 17:46:09 +0900

    アプリからの設定画面呼び出し等

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -3,31 +3,113 @@ package com.example.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.content.ContextCompat;
+import androidx.core.provider.FontRequest;
+import androidx.core.provider.FontsContractCompat;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    private static final String TAG = "MainActivity";
+
+    private Button mRequestDownloadButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES); // アプリ全体に適用
         //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);   // 特定のAcitivityのみ適用
 
+//        View view = getWindow().getDecorView();
+//        view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+//        View view = new View();
+//        view.systemUiVisibility = SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
 
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                                                != PackageManager.PERMISSION_GRANTED){
+//        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+//                                                != PackageManager.PERMISSION_GRANTED){
+//        }
 
-        }
+//        mRequestDownloadButton = findViewById(R.id.button_request);
+//        mRequestDownloadButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String familyName = autoCompleteFamilyName.getText().toString();
+//                if (!isValidFamilyName(familyName)) {
+//                    familyNameInput.setErrorEnabled(true);
+//                    familyNameInput.setError(getString(R.string.invalid_family_name));
+//                    Toast.makeText(
+//                            MainActivity.this,
+//                            R.string.invalid_input,
+//                            Toast.LENGTH_SHORT).show();
+//                    return;
+//                }
+//                requestDownload(familyName);
+//                mRequestDownloadButton.setEnabled(false);
+//            }
+//        });
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private void requestDownload(String familyName) {
+//        QueryBuilder queryBuilder = new QueryBuilder(familyName)
+//                .withWidth(progressToWidth(mWidthSeekBar.getProgress()))
+//                .withWeight(progressToWeight(mWeightSeekBar.getProgress()))
+//                .withItalic(progressToItalic(mItalicSeekBar.getProgress()))
+//                .withBestEffort(mBestEffort.isChecked());
+//        String query = queryBuilder.build();
+        String query = "Noto Sans&weight=400&width=100.0&italic=0.0&besteffort=true";
+
+        Log.d(TAG, "Requesting a font. Query: " + query);
+//        FontRequest request = new FontRequest(
+//                "com.google.android.gms.fonts",
+//                "com.google.android.gms",
+//                query,
+//                R.array.com_google_android_gms_fonts_certs);
+
+//        final ProgressBar progressBar = findViewById(R.id.progressBar);
+//        progressBar.setVisibility(View.VISIBLE);
+
+        FontsContractCompat.FontRequestCallback callback = new FontsContractCompat
+                .FontRequestCallback() {
+            @Override
+            public void onTypefaceRetrieved(Typeface typeface) {
+//                mDownloadableFontTextView.setTypeface(typeface);
+//                progressBar.setVisibility(View.GONE);
+                mRequestDownloadButton.setEnabled(true);
+            }
+
+            @Override
+            public void onTypefaceRequestFailed(int reason) {
+//                Toast.makeText(MainActivity.this,
+//                        getString(R.string.request_failed, reason), Toast.LENGTH_LONG)
+//                        .show();
+//                Toast.makeText(MainActivity.this,
+//                        getString(R.string.request_failed, reason), Toast.LENGTH_LONG)
+//                        .show();
+//                progressBar.setVisibility(View.GONE);
+                mRequestDownloadButton.setEnabled(true);
+            }
+        };
+//        FontsContractCompat
+//                .requestFont(MainActivity.this, request, callback,
+//                        getHandlerThreadHandler());
+    }
+
+
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
@@ -59,8 +141,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void darkthema(View view) {
+        Log.d("BuildConfig.DEBUG","darkmode");
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-//        <style name="AppTheme" parent="Theme.AppCompat.DayNight"></style>
     }
 
 }
